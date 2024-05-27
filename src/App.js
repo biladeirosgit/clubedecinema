55256cd4c0f145faa9bcc81f13cc3ef16270f0d5
bila-olimpiadas <bilaolimpiadas@gmail.com>
2024-05-27 15:11:34 +0100

    pff dá

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from "react-router-dom";
 import CinemaClubPage from './cdc/CinemaClubPage';
 import CinemaClubStats from './cdc/CinemaClubStats'; // Importe o novo componente de estatísticas
 import UserStats from './cdc/UserStats'; // Importe o novo componente de estatísticas de usuário
@@ -10,14 +11,15 @@ import UserStats from './cdc/UserStats'; // Importe o novo componente de estatí
 
 const App = () => {
     return (
-        <Router>
+        
+        <HashRouter>
             <Routes>
                 <Route path="/clubedecinema" element={<CinemaClubPage />} />
                 <Route path="/clubedecinema/stats" element={<CinemaClubStats />} />
                 <Route path="/clubedecinema/users/:username" element={<UserStats />} />
                 {/* Adicione outras rotas aqui */}
             </Routes>
-        </Router>
+        </HashRouter>
     );
 }
 
